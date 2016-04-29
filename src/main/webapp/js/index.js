23f1d249f8a3e007fe5b12178c42c9304c1bbc41
congfeng <congfeng@91nongye.com>
2016-04-29 14:35:10 +0800

    init

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('.p_line').click(function(){
 		var pt = $(this).find('.p_t').text();
-		window.location.href = '/product.html?pt='+pt;
+		window.location.href = 'product.html?pt='+pt;
 	});
 })
