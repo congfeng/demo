50ec9b7571563e078aeb44348dbb95b8fd314cbc
congfeng <congfeng@91nongye.com>
2016-04-29 11:01:00 +0800

    init

---
+++ src/main/java/com/cf/code/web/ProfileController.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.cf.code.web;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cf.code.core.exception.BusinessException;
+import com.cf.code.dao.MenuDao;
+import com.cf.code.dao.UserDao;
+import com.cf.code.entity.Profile;
+import com.cf.code.entity.User;
+import com.cf.code.web.access.AccessVerifier;
+
+/**
+ * @Version: 1.0
+ * @Author: 丛峰
+ * @Email: 3024992@qq.com
+ */
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+	
+	@Resource(name = "userDaoRead")
+	UserDao userDaoRead;
+	
+	@Resource(name = "menuDaoRead")
+	MenuDao menuDaoRead;
+	
+	@RequestMapping(value = {"login"}, method = { RequestMethod.GET,RequestMethod.POST})
+	@ResponseBody
+    public Profile login(Model model,HttpSession session,
+			@RequestParam(required = true) String username,
+			@RequestParam(required = true) String password) throws BusinessException{
+		User user = userDaoRead.find(username);
+		if(user == null){
+			throw new BusinessException("用户不存在");
+		}
+		if(!user.getPassword().equals(password)){
+			throw new BusinessException("用户或密码错误");
+		}
+		List<String> menus = menuDaoRead.query(user.getId());
+		if(menus.isEmpty()){
+			throw new BusinessException("用户无访问权限");
+		}
+		Profile profile = new Profile(session.getId(), user.getId(), user.getUsername(),menus);
+		session.setAttribute("profile", profile);
+        return profile;
+    }
+	
+	@RequestMapping(value = {"logout"}, method = { RequestMethod.GET,RequestMethod.POST})
+	@ResponseBody
+    public void logout(HttpSession session){
+		session.removeAttribute("profile");
+	}
+	
+	@AccessVerifier
+	@RequestMapping(value = {""}, method = { RequestMethod.GET,RequestMethod.POST})
+	@ResponseBody
+    public Profile init(@RequestParam(required = false)Profile profile,HttpSession session) {
+        return profile;
+    }
+	
+}
